546e26be430936d8b8920a998dece1d2875a7987
Ronish Shrestha <stharonish@gmail.com>
2017-03-16 12:25:29 +0545

    setting up table for itinerary

---
+++ resources/views/backend/itinerary/single_itinerary.blade.php
@@ -0,0 +1,64 @@
+@extends('backend.layouts.index')
+@section('content')
+    <style>
+        .btn-edit{
+            background-color: dodgerblue;
+            border: none;
+            border-radius: 3px;
+            height: 25px;
+        }
+        .btn-edit a{
+            color: ghostwhite;
+        }
+        .btn-delete{
+            background-color: #e53935;
+            border: none;
+            border-radius: 3px;
+            height: 25px;
+        }
+        .btn-delete a{
+            color: ghostwhite;
+        }
+        .single-button{
+            width: 100px;
+            margin-left:auto;
+            margin-right: auto;
+        }
+    </style>
+    <div class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+
+                <div class="col-lg-12 col-md-12">
+                    <div class="card">
+                        <div class="card-header" data-background-color="orange">
+                            <h4 class="title" align="center">{{ ucfirst($banner->title) }} | Last modified by: {{ $banner->user->name }}</h4>
+                            <p class="category" align="center">Last updated on
+                                    {{ $banner->updated_at }}
+                            </p>
+                        </div>
+                        <div class="card-content table-responsive">
+                            <p align="center">{{ $banner->description }}</p>
+
+                            @if(Storage::disk('local')->has($banner->image))
+                                <section class="row">
+                                    <div class="col-md-6 col-md-offset-3">
+                                        <img src="{{ route('backend.banner.image', ['filename' => $banner->image]) }}" alt="" class="img-responsive" style="border-radius: 2px;">
+                                    </div>
+                                </section>
+                                <br>
+                            @endif
+
+                            <div class="single-button">
+                            <button class="btn-edit"><a href="{{ route('backend.banner.get.update', ['banner_id' => $banner->id]) }}">Edit</a></button>
+                            <button class="btn-delete"><a href="{{ route('backend.banner.trash', ['banner_id' => $banner->id]) }}">Delete</a></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
